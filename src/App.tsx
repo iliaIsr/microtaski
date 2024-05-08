a417ab4250c5cb94ccaa0257e28e821b2e32de3f
iliaIsr <ilil.sbl91@gmail.com>
2024-05-08 14:52:21 +0300

    1sprint 4lesson

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React, {useState} from "react";
 import './App.css';
+import {ComponentLess4} from "./dzPo4video/ComponentLess4";
 import {Button} from "./Button";
 
 
+export type FilterMoneyType = "All" | "RUBLS" | 'Dollars';
+
 function App() {
+
     let students = [
         {id: 1, name: "James", age: 8},
         {id: 2, name: "Robert", age: 18},
@@ -21,63 +25,65 @@ function App() {
         {id: 14, name: "John", age: 28}
 
     ];
-
-
-
     const Button1foo = (subscraber: string, age: number) => {
         alert(subscraber + " " + age)
     }
     const Button2foo = (subscraber: string, age: number) => {
         alert(subscraber + " " + age)
     }
-    const Button3foo =(nameBut:string)=>{
+    const Button3foo = (nameBut: string) => {
         alert(nameBut)
     }
     // lesson 3
-    let[a, setA]=useState(1)
-    const onCliskHundler1=()=>{
+    let [a, setA] = useState(1)
+    const onCliskHundler1 = () => {
         setA(++a);
         alert(a)
     }
-    const onCliskHundler2=()=>{
+    const onCliskHundler2 = () => {
         setA(0);
         alert(a)
     }
-    const [money, setMoney] = useState([
-        { banknots: 'Dollars', value: 100, number: ' a1234567890' },
-        { banknots: 'Dollars', value: 50, number: ' z1234567890' },
-        { banknots: 'RUBLS', value: 100, number: ' w1234567890' },
-        { banknots: 'Dollars', value: 100, number: ' e1234567890' },
-        { banknots: 'Dollars', value: 50, number: ' c1234567890' },
-        { banknots: 'RUBLS', value: 100, number: ' r1234567890' },
-        { banknots: 'Dollars', value: 50, number: ' x1234567890' },
-        { banknots: 'RUBLS', value: 50, number: ' v1234567890' },
+
+    //lesson4
+    const [money] = useState([
+        {banknots: 'Dollars', value: 100, number: ' a1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' z1234567890'},
+        {banknots: 'RUBLS', value: 100, number: ' w1234567890'},
+        {banknots: 'Dollars', value: 100, number: ' e1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' c1234567890'},
+        {banknots: 'RUBLS', value: 100, number: ' r1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' x1234567890'},
+        {banknots: 'RUBLS', value: 50, number: ' v1234567890'},
     ])
 
-        let currentMoney=money.filter(maney=>maney.banknots==="RUBLS")
+    const [val, setFilter] = useState<FilterMoneyType>("All")
+    console.log(val)
+    let currentMoney = money;
+    if (val === "RUBLS") {
+        currentMoney = money.filter(maney => maney.banknots === "RUBLS")
+    }
+    if (val === "Dollars") {
+        currentMoney = money.filter(maney => maney.banknots === "Dollars")
+    }
+
+    const filterVal = (newValue: FilterMoneyType)=> {
+        setFilter(newValue)
+    }
+
     return (
         <div>
-            <ul>
-                {currentMoney.map((mon, index)=>{
-                    return(
+            <ComponentLess4 filterVal={setFilter} currentMoney={currentMoney}/>
 
-                <li key={index}>
-                    <span>{mon.banknots}</span>
-                    <span>{mon.value}</span>
-                    <span>{mon.number}</span>
-                </li>
-                    )
-                })}
-            </ul>
-            
-            <h1>{a}</h1>
-            <button onClick={onCliskHundler1}>number</button>
-            <button onClick={onCliskHundler2}>0</button>
-            <Button name={"youtube chanel-1"} callBack={() => Button1foo("Vasya", 21)}/>
-            <Button name={"youtube chanel-2"} callBack={() => Button2foo("Jenya", 22)}/>
-            <Button name={"i'am stuped button"} callBack={()=>Button3foo("I'am stupped button")}/>
+            <>
+                <h1>{a}</h1>
+                <button onClick={onCliskHundler1}>number</button>
+                <button onClick={onCliskHundler2}>0</button>
+                <Button name={"youtube chanel-1"} callBack={() => Button1foo("Vasya", 21)}/>
+                <Button name={"youtube chanel-2"} callBack={() => Button2foo("Jenya", 22)}/>
+                <Button name={"i'am stuped button"} callBack={() => Button3foo("I'am stupped button")}/>
+            </>
         </div>
-
 
     );
 }
